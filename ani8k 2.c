1859db03f867333a6d16ab441393e57908a95514
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-02-18 07:45:49 +0600

    c home codes

---
+++ ani8k 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+    char x ;
+    scanf("%c",&x);
+    if(x=='a'){
+        printf("vowel\n");
+    }
+    else if(x=='e'){
+        printf("vowel\n");
+    }
+       else if(x=='i'){
+        printf("vowel\n");
+    }
+    else if(x=='o'){
+        printf("vowel\n");
+    }
+    else if(x=='u'){
+        printf("vowel\n");
+    }
+    else {
+        printf("it is consonant\n");
+    }
+}
---
+++ anik.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int murgi_1,murgi_2,murgi_3,total_murgi;
+    printf("inter the murgis boobs\n");
+    scanf("%d%d%d", &murgi_1, &murgi_2, &murgi_3);
+    total_murgi=murgi_1+murgi_2+murgi_3;
+    printf("the value of total boobs are %d",total_murgi);
+    return 0;
+}
