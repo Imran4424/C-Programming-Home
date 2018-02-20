22cccf4f9945efc3ee45ea2ccfe46da612f1aefc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-02-20 08:41:50 +0600

    character intro

---
+++ character.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	char ch;
+	printf("enter some character\n");
+	scanf("%c%c%c%c%c",&ch,&ch,&ch,&ch,&ch);
+	printf("the character you entered %c%c%c%c%c\n",ch,ch,ch,ch,ch);
+	return 0;
+}
