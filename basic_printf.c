36c0801ea6a759785f94f54e05a958fbe65aaf33
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-02-21 23:14:57 +0600

    basic printf

---
+++ basic_printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+printf("\n\n\rthank you imran for assisting me..............\n");
+return 0;
+}
---
+++ basic_printf2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{int x,y,z;
+printf("enter the value x\n");
+scanf("%d",&x);
+printf("enter the value ofy\n");
+scanf("%d",&y);
+z=x+y;
+printf("the value of %d+%d equals %d",x,y,z);
+return 0;
+
+
+}
