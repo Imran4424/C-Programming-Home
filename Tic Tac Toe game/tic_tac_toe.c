91fd2adbabdfd8c27a7a25adfefc787ce4837073
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-03 18:09:21 +0600

    Tic Tac game implementation

--- Tic Tac Toe game/tic_tac_toe.c
+++ Tic Tac Toe game/tic_tac_toe.c
@@ -1,82 +1,90 @@
 #include <stdio.h>
 
-int main()
+
+void print_current_gameboard(char gameboard[3][3])
 {
-    char gameboard[3][3];
-    int d,i,room,row,col;
-    for(row=0;row<3;row++){
-        for(col=0;col<3;col++){
-            gameboard[row][col]='n';
-        }
-    }
-    for(row=0;row<3;row++){
-        for(col=0;col<3;col++){
+    int row,col;
+
+    for(row=0; row<3; row++)
+    {
+        for(col=0; col<3; col++)
+        {
             printf("\t%c",gameboard[row][col]);
         }
         printf("\n\n");
     }
+}
+
+
+int main()
+{
+    char gameboard[3][3];
+    int d,i,room,row,col;
+
+    for(row=0; row<3; row++)
+    {
+        for(col=0; col<3; col++)
+        {
+            gameboard[row][col]='N';
+        }
+    }
+
+    print_current_gameboard(gameboard);
+    
     printf("Insert 1 to play player X first\n");
     printf("Insert 0 to play player O first\n");
     scanf("%d",&d);
 
-    if(d==1){
-        for(i=1;i<=9;i++){
-            if(i%2==1){
-                printf("insert room no\n");
+    if(d==1)
+    {
+        for(i=1; i<=9; i++)
+        {
+            if(i%2==1)
+            {
+                printf("insert room no for player X\n");
                 scanf("%d",&room);
                 row=(room-1)/3;
                 col=(room-1)%3;
                 gameboard[row][col]='X';
-                for(row=0;row<3;row++){
-                    for(col=0;col<3;col++){
-                        printf("\t%c",gameboard[row][col]);
-                    }
-                    printf("\n\n");
-                }
+
+                print_current_gameboard(gameboard);
             }
-            else if(i%2==0){
-                printf("insert room no\n");
+            else if(i%2==0)
+            {
+                printf("insert room no for player O\n");
                 scanf("%d",&room);
                 row=(room-1)/3;
                 col=(room-1)%3;
                 gameboard[row][col]='O';
-                for(row=0;row<3;row++){
-                    for(col=0;col<3;col++){
-                        printf("\t%c",gameboard[row][col]);
-                    }
-                    printf("\n\n");
-                }
+                
+                print_current_gameboard(gameboard);
             }
         }
     }
 
-    else if(d==0){
-        for(i=1;i<=9;i++){
-            if(i%2==0){
-                printf("insert room no\n");
+    else if(d==0)
+    {
+        for(i=1; i<=9; i++)
+        {
+            if(i%2==0)
+            {
+                printf("insert room no for player O\n");
                 scanf("%d",&room);
                 row=(room-1)/3;
                 col=(room-1)%3;
                 gameboard[row][col]='X';
-                for(row=0;row<3;row++){
-                    for(col=0;col<3;col++){
-                        printf("\t%c",gameboard[row][col]);
-                    }
-                    printf("\n\n");
-                }
+                
+                print_current_gameboard(gameboard);
             }
-            else if(i%2==1){
-                printf("insert room no\n");
+            else if(i%2==1)
+            {
+                printf("insert room no for player X\n");
                 scanf("%d",&room);
                 row=(room-1)/3;
                 col=(room-1)%3;
                 gameboard[row][col]='O';
-                for(row=0;row<3;row++){
-                    for(col=0;col<3;col++){
-                        printf("\t%c",gameboard[row][col]);
-                    }
-                    printf("\n\n");
-                }
+                
+                print_current_gameboard(gameboard);
             }
         }
     }
