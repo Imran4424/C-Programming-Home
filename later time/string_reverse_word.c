2cec62ea944727e4d2bcc193407eac9508e864c4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-24 23:02:31 +0600

    c codes

--- later time/string_reverse_word.c
+++ later time/string_reverse_word.c
@@ -18,6 +18,16 @@ int str_len(char str[])
     return length;
 }
 
+int chk_character(char c)
+{
+    if(c == ' ' || c == ',' || c == ';' || c == '!')
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
 
 int main()
 {
@@ -27,18 +37,28 @@ int main()
 
     char reverse_str[len];
 
-    int i,j,k;
+    int i,j,k = 0;
 
-    for(i=0; i <len;i++)
+    for(i = 0;str[i] != '\0';i++)
     {
-        if(str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '!')
+        if(chk_character(str[i]) || str[i+1] == '\0')
         {
-            for(j=i-1;str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '!'|| j < 0 ; j--)
+
+            for(j = i-1;chk_character(str[j])|| j >= 0 ; j--)
             {
 
+
+                reverse_str[k] = str[j];
+
+                k++;
             }
+
+            reverse_str[k] = ' ';
+            k++;
         }
     }
+
+    printf("%s\n",reverse_str);
 
     return 0;
 }
