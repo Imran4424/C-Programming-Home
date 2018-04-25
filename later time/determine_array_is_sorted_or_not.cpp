be5e5f9c5c4d09b62c3d34e3641191591c435771
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-26 05:28:27 +0600

    c home

--- later time/determine_array_is_sorted_or_not.cpp
+++ later time/determine_array_is_sorted_or_not.cpp
@@ -3,6 +3,38 @@
 
 typedef long long int lli;
 
+int ascending_chk(int ar[], int ar_size)
+{
+    int min_val = ar[0];
+
+    for(int i=0;i < ar_size; i++)
+    {
+        if(ar[i] < min_val)
+        {
+            return 0;
+        }
+        else
+        {
+            min_val = ar[i];
+        }
+    }
+
+    return 1;
+}
+
+int decending_chk(int ar[], int ar_size)
+{
+    int max_val = ar[0];
+
+    for(int )
+}
+
+
+int array_sor_chk(int ar[], int ar_size)
+{
+
+}
+
 int main()
 {
     int n;
@@ -11,6 +43,22 @@ int main()
     scanf("%d",&n);
 
     int ar[n];
+
+    printf("Now input the array\n");
+
+    for(int i = 0; i<n; i++) ///here ++i or i++ does not matter at all, the same effect will happen
+    {
+        scanf("%d", &ar[i]);
+    }
+
+    if(array_sor_chk(ar,n))
+    {
+        printf("Array is sorted\n");
+    }
+    else
+    {
+        printf("Array is not sorted\n");
+    }
 
     retrun 0;
 }
