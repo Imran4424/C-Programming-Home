84d791947ad109fbd4f8e8fe71e99587ebce53eb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-29 21:28:17 +0600

    string codes

--- later time/sub_string.c
+++ later time/sub_string.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
 
+int string_size(char str[])
+{
+    int i,sz = 0;
+
+    for(i=0;str[i] != '/0';i++)
+    {
+        sz++;
+    }
+
+    return sz;
+}
+
+
 int main()
 {
     char str[50],str2[50];
 
+    int i,j;
+
     scanf("%[^\n]%*c",str);
     scanf("%[^\n]%*c",str2);
 
-
+    str_len = string_s
 
     return 0;
 }
