8e0d6110bf8a8cc14929270db6022589754d45e9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-30 00:03:18 +0600

    string codes

---
+++ later time/sub_string_2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i, j, len1, len2;
+    char str1[56],str2[46];
+
+    printf("Enter a string: ");
+    gets(str1);
+    printf("Enter the sub string: ");
+    gets(str2);
+
+    len1 = strlen(str1);
+    len2 = strlen(str2);
+
+    for(i = 0; i <(len1 - len2); i++)
+    {
+
+        if(str2[0] == str1[i])
+        {
+
+            for(j = 1; j < len2; j++)
+            {
+
+                if(str2[j] != str1[i+j])
+                {
+                    printf()
+
+                    break;
+                }
+            }
+            if(j  == len2)
+            {
+
+                printf("%d\n", i);
+                printf("sub string is found\n");
+                break;
+            }
+        }
+
+    }
+    return 0;
+}
