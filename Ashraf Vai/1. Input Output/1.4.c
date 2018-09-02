c1621622af1be7aa0f901988c003c86cbfe55770
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-02 07:41:32 +0600

    ashraf vai home

--- Ashraf Vai/1. Input Output/1.4.c
+++ Ashraf Vai/1. Input Output/1.4.c
@@ -12,13 +12,13 @@ int main()
 
 	printf("%f\n", num); //default
 
-	printf("%.1f\n", num); // 1 precision
+	printf("%.1f\n", num); 
 
-	printf("%.2f\n", num); // 2 precision
+	printf("%.2f\n", num);
 
-	printf("%.3f\n", num); // 3 precision
+	printf("%.3f\n", num); 
 	
-	printf("%.4f\n", num); // 4 precision
+	printf("%.4f\n", num); 
 
 	return 0;
 }
